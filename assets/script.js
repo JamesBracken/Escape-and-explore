177d6c42d444c30230815ee55e24300f84b877de
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-01-01 21:25:33 +0000

    add: moved user location into an object

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,32 @@
 API_KEY = "AIzaSyB4fcJiibwlX8tcRe5dNnllSifqBCKqeqA";
 API_URL = ""
 
-let currentUserLongitude;
-let currentUserLatitude;
-let successCallBack = (position) => {
-  currentUserLongitude = position.coords.longitude;
-  currentUserLatitude = position.coords.latitude;
-  console.log(position);
-  console.log(currentUserLongitude);
-  console.log(currentUserLatitude);
+// Global variables
+let userDetails = {
+  currentUserLongitude: -25.344,
+  currentUserLatitude: 131.031,
+  successCallBack: (position) => {
+    currentUserLongitude = position.coords.longitude;
+    currentUserLatitude = position.coords.latitude;
+    console.log(position);
+    console.log(currentUserLongitude);
+    console.log(currentUserLatitude);
+  },
+  errorCallBack: console.log("error"),
+
 }
+// let currentUserLongitude;
+// let currentUserLatitude;
+// let successCallBack = (position) => {
+//   currentUserLongitude = position.coords.longitude;
+//   currentUserLatitude = position.coords.latitude;
+//   console.log(position);
+//   console.log(currentUserLongitude);
+//   console.log(currentUserLatitude);
+// }
 let errorCallBack = console.log("error")
-let userCurrentLocation = navigator.geolocation.getCurrentPosition(successCallBack, errorCallBack)
-console.log(currentUserLongitude + currentUserLatitude)
+let userCurrentLocation = navigator.geolocation.getCurrentPosition(userDetails.successCallBack, userDetails.errorCallBack)
+console.log(userDetails.currentUserLongitude + userDetails.currentUserLatitude)
 
 // Code to initialise, add and update google maps api 
 // Copy pasted from google official documentation and then tweaked to suit website needs
