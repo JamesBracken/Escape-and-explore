24b444fbf7718ba572472e0fbd7a1b7ac55318d8
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-01-01 22:52:51 +0000

    Fix: Corrected paths and organised objects

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,51 @@
 API_KEY = "AIzaSyB4fcJiibwlX8tcRe5dNnllSifqBCKqeqA";
 API_URL = ""
 
-// Global variables
+// GLOBAL VARIABLES
+let map;
 let userDetails = {
-  currentUserLatitude: 131.031,
-  currentUserLongitude: -25.344,
+  currentUserLatitude: 20,
+  currentUserLongitude: 20,
   successCallBack: (position) => {
-    currentUserLongitude = position.coords.longitude;
-    currentUserLatitude = position.coords.latitude;
-    chosenCityLatitude = currentUserLatitude;
-    chosenCityLatitude = currentUserLongitude;
+    userDetails.currentUserLongitude = position.coords.longitude;
+    userDetails.currentUserLatitude = position.coords.latitude;
+    chosenCityDetails.chosenCityLatitude = userDetails.currentUserLatitude;
+    chosenCityDetails.chosenCityLongitude = userDetails.currentUserLongitude;
+console.log("lat:" + userDetails.currentUserLatitude)
+console.log("lon:" + userDetails.currentUserLongitude)
     initMap();
   },
   errorCallBack: console.log("error"),
-
 }
 let chosenCityDetails = {
-  chosenCityLatitude: 0.0,
-  chosenCityLongitude: 0.0,
+  chosenCityLatitude: userDetails.currentUserLatitude,
+  chosenCityLongitude: userDetails.currentUserLongitude,
 }
+let ;
+}
+document.getElementsByClassName()
 
-// let currentUserLongitude;
 // let currentUserLatitude;
 // let successCallBack = (position) => {
 //   currentUserLongitude = position.coords.longitude;
 //   currentUserLatitude = position.coords.latitude;
-//   console.log(position);
 //   console.log(currentUserLongitude);
-//   console.log(currentUserLatitude);
-// }
-let userCurrentLocation = navigator.geolocation.getCurrentPosition(userDetails.successCallBack, userDetails.errorCallBack)
-console.log(userDetails.currentUserLatitude)
-console.log(userDetails.currentUserLongitude)
+navigator.geolocation.getCurrentPosition(userDetails.successCallBack, userDetails.errorCallBack)
 
 // Code to initialise, add and update google maps api 
 // Copy pasted from google official documentation and then tweaked to suit website needs
-let map;
 
 async function initMap() {
-  // The location of Uluru
-  // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
     zoom: 4,
     center: {lat: chosenCityDetails.chosenCityLatitude , lng: chosenCityDetails.chosenCityLongitude },
     mapId: "DEMO_MAP_ID",
   });
 
-  // The marker, positioned at Uluru
   const marker = new AdvancedMarkerElement({
     map: map,
     position: {lat: chosenCityDetails.chosenCityLatitude , lng: chosenCityDetails.chosenCityLongitude },
