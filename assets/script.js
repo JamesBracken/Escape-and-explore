979c1ddcbafea22e853f252e1bb217c366e35d45
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-01-02 20:36:08 +0000

    Add: city data

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,23 @@ API_KEY = "AIzaSyB4fcJiibwlX8tcRe5dNnllSifqBCKqeqA";
 API_URL = ""
 
 // GLOBAL VARIABLES
+
+// data for every city
+let cities = {
+  parisCity: {
+    cityName: "Paris",
+    cityDescription: "Paris fill content",
+    cityLatitude: 48.856566667515224,
+    cityLongitude: 2.355958775816939,
+  },
+  londonCity: {
+    cityName: "London",
+    cityDescription: "London fill content",
+    cityLatitude: 51.50758961962817,
+    cityLongitude: -0.12083456114608788,
+  }
+}
+
 let map;
 let userDetails = {
   currentUserLatitude: 20,
@@ -65,7 +82,6 @@ async function handleCityClick() {
   cityInformationContainer.innerHTML = `<h1>The code worked</h1> <br>
     <p>This would be content about the chosen city</p>`
 }
-// Get user location
-// Use user location to set initial location of map if permission is allowed
-// Display warning if location permissions is denied that this will affect the functionality of the website
 // Re prompt the user on warning to enable locations
+// Fix event listener to update  location on city click
+// Add data pull into handleCityClick function based on which city is clicked
