55f21f0d88da1de58bfc16d9ac227536ab6286e9
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-01-03 00:59:13 +0000

    Add: missing semi colons

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 API_KEY = "AIzaSyB4fcJiibwlX8tcRe5dNnllSifqBCKqeqA";
-API_URL = ""
+API_URL = "";
 
 // GLOBAL VARIABLES
 
@@ -17,7 +17,7 @@ let cities = {
     cityLatitude: 51.50758961962817,
     cityLongitude: -0.12083456114608788,
   }
-}
+};
 
 let map;
 let userDetails = {
@@ -29,40 +29,38 @@ let userDetails = {
     userDetails.currentUserLatitude = position.coords.latitude;
     chosenCityDetails.chosenCityLatitude = userDetails.currentUserLatitude;
     chosenCityDetails.chosenCityLongitude = userDetails.currentUserLongitude;
-    console.log("lat:" + userDetails.currentUserLatitude)
-    console.log("lon:" + userDetails.currentUserLongitude)
     initMap();
   },
   errorCallBack: () => {
     console.log("error")
     alert("If user location permissions are denied, not all features of this website will be available. Please update your location permissions and then resfresh this page")
   }
-}
+};
 // On click of a city or on approval of user location permissions this will update the chosen cities position attributes
 // On update of these attributes google maps API location is updated 
 let chosenCityDetails = {
   chosenCityLatitude: userDetails.currentUserLatitude,
   chosenCityLongitude: userDetails.currentUserLongitude,
-}
+};
 
 // CLICK HANDLERS
 let images = document.getElementsByClassName("cityImageButton")
-for (let image of images) {
+for (image of images) {
   image.addEventListener("click", handleCityClick)
-}
+};
 // USER LOCATION UPDATING
 
 // When website is opened, the below code prompts the user to give the website location permissions
 navigator.geolocation.getCurrentPosition(userDetails.successCallBack, userDetails.errorCallBack)
 
-// Code to initialise, add and update google maps API 
+// Code to initialise, add and update google maps API
 // Copy pasted from google official documentation and then tweaked to suit the websites needs
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
   map = new Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 12,
     center: { lat: chosenCityDetails.chosenCityLatitude, lng: chosenCityDetails.chosenCityLongitude },
     mapId: "DEMO_MAP_ID",
   });
@@ -71,8 +69,8 @@ async function initMap() {
     map: map,
     position: { lat: chosenCityDetails.chosenCityLatitude, lng: chosenCityDetails.chosenCityLongitude },
     title: "Uluru",
-  });
-}
+  })
+};
 
 initMap();
 
@@ -87,7 +85,14 @@ async function handleCityClick(e) {
   let cityInformationContainer = document.getElementById("cityInformationContainer");
   cityInformationContainer.innerHTML = `<h1>The code worked</h1> <br>
     <p>This would be content about the chosen city</p>`
-}
+};
+
+// -------------------------------------------------------------------
+// TASKS
+
+// Fix zoom, add data to each city and make this adjust dynamically
+// Add a permanent marker for user location, preferrably different to the normal marker, probably by color
+
+
+// MAYBE
 // Re prompt the user on warning to enable locations
-// Fix event listener to update  location on city click
-// Add data pull into handleCityClick function based on which city is clicked
