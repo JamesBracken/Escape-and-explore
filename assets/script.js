a0cfe1ed79fa583b19ef9713705dcfa6174f5bc3
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-01-05 16:49:26 +0000

    Add: parisCity data

--- assets/script.js
+++ assets/script.js
@@ -14,8 +14,23 @@ let cities = {
     cityPlacesToVisit: [{
       place: "Eiffel tower",
       image: "./assets/images/paris_eiffel_tower.webp",
-      description: "The lovely eiffel tower, how fantastique",
-
+      description: "The iconic symbol of Paris, the Eiffel Tower offers breathtaking views of the city from its observation decks. Completed in 1889, it stands as a testament to architectural innovation and attracts millions of visitors annually.",
+      findOutMore: "https://en.wikipedia.org/wiki/Eiffel_Tower"
+    },{
+      place: "Louvre Museum",
+      image: "./assets/images/paris_eiffel_tower.webp",
+      description: "Home to thousands of masterpieces, including the Mona Lisa and the Venus de Milo, the Louvre is the world’s largest art museum. Its stunning glass pyramid entrance is a modern architectural landmark in the heart of Paris.",
+      findOutMore: "https://en.wikipedia.org/wiki/Louvre"
+    },{
+      place: "Notre-Dame Cathedral",
+      image: "./assets/images/paris_eiffel_tower.webp",
+      description: "This Gothic masterpiece, located on the Île de la Cité, is renowned for its stunning architecture, rose windows, and historic significance. Though under restoration after the 2019 fire, it remains a symbol of Parisian heritage.",
+      findOutMore: "https://en.wikipedia.org/wiki/Notre-Dame_de_Paris"
+    },{
+      place: "Sacré-Cœur Basilica",
+      image: "./assets/images/paris_eiffel_tower.webp",
+      description: "is a Roman Catholic church on Montmartre hill, known for its striking white domes. Completed in 1914, it features a large mosaic inside and offers panoramic views of Paris from its terrace. It’s a symbol of hope and a popular site for both worship and tourism.",
+      findOutMore: "https://en.wikipedia.org/wiki/Sacr%C3%A9-C%C5%93ur,_Paris"
     },
     ]
   },
@@ -30,19 +45,19 @@ let cities = {
       image: "./assets/images/london_big_ben.jpg",
       description: "Big Ben, officially the Elizabeth Tower, is an iconic symbol of London at the Palace of Westminster. Built in 1859, its towering Gothic design and clock face are globally recognized, offering a classic view from Westminster Bridge.",
       findOutMore: "https://en.wikipedia.org/wiki/Big_Ben"
-    }, {    
+    }, {
       place: "The British Museum",
-      image: "./assets/images/london_big_ben.jpg",
+      image: "./assets/images/british_museum.jpg",
       description: "The British Museum, located in Bloomsbury, is one of the world’s largest museums, showcasing human history, art, and culture. Opened in 1753, it houses iconic artifacts like the Rosetta Stone and the Elgin Marbles. With free entry, it offers a fascinating journey through over two million years of history.",
       findOutMore: "https://en.wikipedia.org/wiki/British_Museum"
     }, {
       place: "Tower of London",
-      image: "./assets/images/london_big_ben.jpg",
+      image: "./assets/images/tower_of_london.webp",
       description: "The Tower of London, a historic fortress on the Thames, has been a royal palace, prison, and treasury since 1066. It’s home to the Crown Jewels and guarded by the Yeoman Warders. Visitors can explore its medieval structures and learn about its rich and sometimes eerie past.",
       findOutMore: "https://en.wikipedia.org/wiki/Tower_of_London"
     }, {
       place: "Covent Garden",
-      image: "./assets/images/london_big_ben.jpg",
+      image: "./assets/images/covent_garden.webp",
       description: "Covent Garden, in London’s West End, is a lively district famous for its historic market, boutique shops, and vibrant street performances. It’s a hub for entertainment, featuring the Royal Opera House and an array of restaurants and theaters.",
       findOutMore: "https://en.wikipedia.org/wiki/Covent_Garden"
     },
@@ -144,7 +159,7 @@ async function displayCityInformation() {
     <div class="card mb-3" style="max-width: 740px;">
       <div class="row g-0">
         <div class="col-md-4">
-          <img src="${cities[cityId].cityPlacesToVisit[cityCount].image}" class="img-fluid rounded-start placesToVisitCardImg" alt="...">
+          <img src="${cities[cityId].cityPlacesToVisit[cityCount].image}" class="img-fluid rounded-start placesToVisitCardImg h-100" alt="...">
         </div>
         <div class="col-md-8">
           <div class="card-body">
