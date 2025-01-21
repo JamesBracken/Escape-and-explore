1d49cc9a0d1e579c73a6a46946adfbc389cf91af
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-01-21 16:06:27 +0000

    Add: selected city outline

--- assets/script.js
+++ assets/script.js
@@ -17,25 +17,25 @@ let cities = {
       image: "./assets/images/paris_eiffel_tower.webp",
       description: "The iconic symbol of Paris, the Eiffel Tower offers breathtaking views of the city from its observation decks. Completed in 1889, it stands as a testament to architectural innovation and attracts millions of visitors annually.",
       findOutMore: "https://en.wikipedia.org/wiki/Eiffel_Tower",
-      alt:"The beautiful Eiffel tower with an amazing sunset behind it"
+      alt: "The beautiful Eiffel tower with an amazing sunset behind it"
     }, {
       place: "Louvre Museum",
       image: "./assets/images/louvre_museum.jpg",
       description: "Home to thousands of masterpieces, including the Mona Lisa and the Venus de Milo, the Louvre is the world’s largest art museum. Its stunning glass pyramid entrance is a modern architectural landmark in the heart of Paris.",
       findOutMore: "https://en.wikipedia.org/wiki/Louvre",
-      alt:"The louvre located in paris, houses in the background and the beautiful blue sky"
+      alt: "The louvre located in paris, houses in the background and the beautiful blue sky"
     }, {
       place: "Notre-Dame Cathedral",
       image: "./assets/images/notre_dame_cathedral.jpg",
       description: "This Gothic masterpiece, located on the Île de la Cité, is renowned for its stunning architecture, rose windows, and historic significance. Though under restoration after the 2019 fire, it remains a symbol of Parisian heritage.",
       findOutMore: "https://en.wikipedia.org/wiki/Notre-Dame_de_Paris",
-      alt:"The notre dame cathedral with trees on the right and the beautiful sky"
+      alt: "The notre dame cathedral with trees on the right and the beautiful sky"
     }, {
       place: "Sacré-Cœur Basilica",
       image: "./assets/images/Sacré-Cœur Basilica.webp",
       description: "is a Roman Catholic church on Montmartre hill, known for its striking white domes. Completed in 1914, it features a large mosaic inside and offers panoramic views of Paris from its terrace. It’s a symbol of hope and a popular site for both worship and tourism.",
       findOutMore: "https://en.wikipedia.org/wiki/Sacr%C3%A9-C%C5%93ur,_Paris",
-      alt:"The Sacré-Cœur Basilica with refreshingly green grass out front and surrounded by nature"
+      alt: "The Sacré-Cœur Basilica with refreshingly green grass out front and surrounded by nature"
     },
     ]
   },
@@ -50,25 +50,25 @@ let cities = {
       image: "./assets/images/london_big_ben.jpg",
       description: "Big Ben, officially the Elizabeth Tower, is an iconic symbol of London at the Palace of Westminster. Built in 1859, its towering Gothic design and clock face are globally recognized, offering a classic view from Westminster Bridge.",
       findOutMore: "https://en.wikipedia.org/wiki/Big_Ben",
-      alt:"The big ben and buildings surrounding it"
+      alt: "The big ben and buildings surrounding it"
     }, {
       place: "The British Museum",
       image: "./assets/images/british_museum.jpg",
       description: "The British Museum, located in Bloomsbury, is one of the world’s largest museums, showcasing human history, art, and culture. Opened in 1753, it houses iconic artifacts like the Rosetta Stone and the Elgin Marbles. With free entry, it offers a fascinating journey through over two million years of history.",
       findOutMore: "https://en.wikipedia.org/wiki/British_Museum",
-      alt:"The british museum and the dreadful british weather on display for all to see "
+      alt: "The british museum and the dreadful british weather on display for all to see "
     }, {
       place: "Tower of London",
       image: "./assets/images/tower_of_london.webp",
       description: "The Tower of London, a historic fortress on the Thames, has been a royal palace, prison, and treasury since 1066. It’s home to the Crown Jewels and guarded by the Yeoman Warders. Visitors can explore its medieval structures and learn about its rich and sometimes eerie past.",
       findOutMore: "https://en.wikipedia.org/wiki/Tower_of_London",
-      alt:"The tower of london, the thames river out front and surrounded by trees"
+      alt: "The tower of london, the thames river out front and surrounded by trees"
     }, {
       place: "Covent Garden",
       image: "./assets/images/covent_garden.webp",
       description: "Covent Garden, in London’s West End, is a lively district famous for its historic market, boutique shops, and vibrant street performances. It’s a hub for entertainment, featuring the Royal Opera House and an array of restaurants and theaters.",
       findOutMore: "https://en.wikipedia.org/wiki/Covent_Garden",
-      alt:"A picture inside covent garden displaying the beauty of the modern building and the christmas balls hanging from the ceiling"
+      alt: "A picture inside covent garden displaying the beauty of the modern building and the christmas balls hanging from the ceiling"
     },
     ]
   }
@@ -255,15 +255,15 @@ let bookingFormInputs = [firstNameInput, lastNameInput, emailInput, confirmEmail
 // const firstNameFeedback = document.getElementById("firstNameFeedback");
 
 // Adding event listeners for our booking form inputs
-for (input of bookingFormInputs) {
-  input.addEventListener("focusout", onBookingFormFocusOut)
-  input.addEventListener("submit", onBookingFormFocusSubmit)
-  input.addEventListener("input", onBookingFormChange)
-  // This is to prevent the html validation messages from showing, we instead want our own validation messages showing
-  input.addEventListener("invalid", (e) => {
-    // e.preventDefault()
-  })
-}
+// for (input of bookingFormInputs) {
+// input.addEventListener("focusout", onBookingFormFocusOut)
+// input.addEventListener("submit", onBookingFormFocusSubmit)
+// input.addEventListener("input", onBookingFormChange)
+// This is to prevent the html validation messages from showing, we instead want our own validation messages showing
+// input.addEventListener("invalid", (e) => {
+// e.preventDefault()
+//   })
+// }
 
 // On submit of the form validation checks within this function are performed
 function onBookingFormFocusSubmit() {
@@ -273,7 +273,7 @@ function onBookingFormFocusSubmit() {
     if (input.value.length === 0 || input.value.length < 1) {
       targetErrorElement.classList.remove("inactiveError")
       input.classList.add("redBorder")
-      console.log("Cycle working")  
+      console.log("Cycle working")
     }
   }
 }
@@ -289,17 +289,35 @@ function onBookingFormFocusOut(e) {
 }
 
 // When there are any buttons press or changes made when in focus in the booking input elements, this function is invoked
-function onBookingFormChange(e)  {
+function onBookingFormChange(e) {
   let targetErrorElement = document.getElementById(`${e.target.id + "EmptyFeedback"}`)
   let targetedElement = e.target
 
 
-  if(e.target.value.length > 0) {
+  if (e.target.value.length > 0) {
     targetErrorElement.classList.add("inactiveError")
     targetedElement.classList.remove("redBorder")
   }
 }
 
+let cityCards = document.getElementsByClassName("cityCard")
+
+for (card of cityCards) {
+  card.addEventListener("click", toggleCityOutline)
+}
+
+function toggleCityOutline(e) {
+  let selectedCityCard = e.target.parentNode
+  let cardOutlinedBoolean = selectedCityCard.classList.contains("cityCardOutlined")
+  if (cardOutlinedBoolean) {
+    selectedCityCard.classList.remove("cityCardOutlined")
+  } else {
+    for (card of cityCards) {
+      card.classList.remove("cityCardOutlined")
+      selectedCityCard.classList.add("cityCardOutlined")
+    }
+  }
+}
 // First name must have data
 // Last name must have data
 
